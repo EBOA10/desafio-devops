3dfddf91b3e75c6d185e9fed5a6b6a00f739b672
eboa10 <ebrunaoa@gmail.com>
2025-09-27 10:00:56 -0300

    Entrega artefatos ajustados

--- infra/main.tf
+++ infra/main.tf
@@ -19,8 +19,8 @@ resource "aws_ecs_cluster" "main" { name = "${var.project_name}-cluster" }
 resource "aws_iam_role" "ecs_task_execution_role" { name = "${var.project_name}-ecs-execution-role"; assume_role_policy = jsonencode({ Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ecs-tasks.amazonaws.com" } }] }) }
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" { role = aws_iam_role.ecs_task_execution_role.name; policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" }
 
-# Definição da Tarefa ECS (o blueprint do nosso contêiner)
+# Definição da Tarefa ECS (o blueprint do contêiner)
 resource "aws_ecs_task_definition" "app" { family = "${var.project_name}-task"; network_mode = "awsvpc"; requires_compatibilities = ["FARGATE"]; cpu = "256"; memory = "512"; execution_role_arn = aws_iam_role.ecs_task_execution_role.arn; container_definitions = jsonencode([{ name = "${var.project_name}-container", image = "eboa10/desafio-devops:latest", essential = true, portMappings = [{ containerPort = 80, hostPort = 80 }] }]) }
 
-# Serviço ECS (responsável por manter nosso contêiner rodando)
+# Serviço ECS (responsável por manter o contêiner rodando)
 resource "aws_ecs_service" "main" { name = "${var.project_name}-service"; cluster = aws_ecs_cluster.main.id; task_definition = aws_ecs_task_definition.app.arn; desired_count = 1; launch_type = "FARGATE"; network_configuration { subnets = [aws_subnet.public.id]; security_groups = [aws_security_group.ecs_service_sg.id]; assign_public_ip = true } }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,23 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources."
+  description = "Região da AWS onde a infraestrutura será criada."
   type        = string
   default     = "us-east-1"
+
+  validation {
+    # Garante que a região informada seja uma das duas opções válidas
+    condition     = contains(["us-east-1", "sa-east-1"], var.aws_region)
+    error_message = "A região da AWS precisa ser 'us-east-1' ou 'sa-east-1'."
+  }
 }
 
-variable "project_name" {
-  description = "The name of the project, used for tagging resources."
+variable "desafio-devops" {
+  description = "Nome do projeto. Usado para nomear e indetificar os recursos criados."
   type        = string
   default     = "php-modernization"
+
+  validation {
+    # Garante que o nome do projeto tenha entre 5 e 20 caracteres
+    condition     = length(var.project_name) >= 5 && length(var.project_name) <= 20
+    error_message = "O nome do projeto deve ter entre 5 e 20 caracteres."
+  }
 }
